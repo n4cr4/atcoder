58c7b5e91331d6b8a927b4852a4aa02d79ace5c0
n4cr4 <itosukenao@gmail.com>
2024-02-23 22:22:23 +0900

    add: tessoku solver

---
+++ solver/tessoku-book/a01/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << n * n << endl;
+    return 0;
+}
