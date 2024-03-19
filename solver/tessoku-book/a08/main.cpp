3470e39ad380c6d36cfc62ab1520ed160b2fb0f8
n4cr4 <itosukenao@gmail.com>
2024-03-19 14:20:43 +0900

    add: hoge

--- solver/tessoku-book/a08/main.cpp
+++ solver/tessoku-book/a08/main.cpp
@@ -8,11 +8,55 @@ using P = pair<int, int>;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int H, W, Q;
+    cin >> H >> W;
 
-    int ans = 0;
+    int X[H][W];
 
-    cout << ans << endl;
+    rep(i, H)
+    {
+        rep(j, W)
+        {
+            cin >> X[i][j];
+        }
+    }
+
+    cin >> Q;
+    int A[Q], B[Q], C[Q], D[Q];
+    rep(i, Q)
+    {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+
+    vector<vector<int>> Z(H + 1, vector<int>(W + 1, 0));
+    for (int i = 1; i <= W; i++)
+    {
+        for (int j = 1; j <= H; j++)
+        {
+            Z[j][i] = Z[j][i - 1] + X[j - 1][i - 1];
+        }
+    }
+    for (int j = 1; j <= H; j++)
+    {
+        for (int i = 1; i <= W; i++)
+        {
+            Z[j][i] = Z[j - 1][i] + Z[j][i];
+        }
+    }
+
+    // for (int j = 1; j <= H; j++)
+    // {
+    //     for (int i = 1; i <= W; i++)
+    //     {
+    //         cout << Z[j][i] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    rep(i, Q)
+    {
+        cout << Z[C[i]][D[i]] - Z[C[i]][B[i] - 1] - Z[A[i] - 1][D[i]] + Z[A[i] - 1][B[i] - 1] << endl;
+    }
+
     return 0;
 }
--- solver/tessoku-book/a09/main.cpp
+++ solver/tessoku-book/a09/main.cpp
@@ -8,11 +8,58 @@ using P = pair<int, int>;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int H, W, N;
+    cin >> H >> W >> N;
 
-    int ans = 0;
+    int A[N], B[N], C[N], D[N];
+    rep(i, N)
+    {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+    int Z[H + 2][W + 2];
+    rep(i, H + 1)
+    {
+        rep(j, W + 1)
+        {
+            Z[i][j] = 0;
+        }
+    }
 
-    cout << ans << endl;
+    rep(i, N)
+    {
+        Z[A[i]][B[i]] += 1;
+        Z[C[i] + 1][D[i] + 1] += 1;
+        Z[A[i]][D[i] + 1] -= 1;
+        Z[C[i] + 1][B[i]] -= 1;
+    }
+
+    for (int i = 1; i <= H; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            Z[i][j] = Z[i - 1][j] + Z[i][j];
+        }
+    }
+
+    for (int j = 1; j <= W; j++)
+    {
+        for (int i = 1; i <= H; i++)
+        {
+            Z[i][j] = Z[i][j - 1] + Z[i][j];
+        }
+    }
+
+    for (int i = 1; i <= H; i++)
+    {
+        for (int j = 1; j <= W; j++)
+        {
+            cout << Z[i][j];
+            if (j != W)
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
